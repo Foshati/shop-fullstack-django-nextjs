aa55d9c5abf8bd3f5b39e2d8c72b01a8c01a6e59
Foshati <amirrezafoshati@gmail.com>
2024-09-06 23:54:10 +0330

    start project with daisy ui and tailwindcss and htmx and django create todo

---
+++ todo/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = "todo"
+urlpatterns = [
+    path("", views.todo_list, name="todo_list"),
+    path("add/", views.add_todo, name="add_todo"),
+    path("delete/<int:pk>/", views.delete_todo, name="delete_todo"),
+    path("toggle/<int:pk>/", views.toggle_todo, name="toggle_todo"),
+]
